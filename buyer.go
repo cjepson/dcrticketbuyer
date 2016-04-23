e7a2df01b79e7329c6ff241c523a0627698533a2
cjepson <cjepson@decred.org>
2016-04-23 15:37:22 -0400

    Add license

--- buyer.go
+++ buyer.go
@@ -1,4 +1,7 @@
-// buyer.go
+// Copyright (c) 2016 The Decred developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
 package main
 
 import (
